d09da619489a4144a63ac4e4da0e9f6b2f53ffa0
Rama Putcha <srinuram@yahoo.com>
2014-06-05 11:52:35 +1000

    Added new button to the panel

--- GWT Hello World/src/org/rama/gwt/client/Home.java
+++ GWT Hello World/src/org/rama/gwt/client/Home.java
@@ -1,18 +1,34 @@
 package org.rama.gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.sun.java.swing.plaf.windows.resources.windows;
 
 public class Home implements EntryPoint {
 
 	Label helloWorldLB = new Label("Hello Rama, This is wonderful World which has given opportunity to learn many new things. Thank you.!");
 	MessageForm msgForm = new MessageForm();
+	Button button = new Button("New Button in GWT");
 	
 	@Override
 	public void onModuleLoad() {
 		RootPanel.get().add(helloWorldLB);
 		RootPanel.get().add(msgForm);
+		RootPanel.get().add(button);
+		
+		button.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				Window.alert("Button is Clicked");
+				
+			}
+		});
 	}
 
 }
